d99c8d78885713b79aa43f017dca982dfaefc608
tansen <tansenkhan1990@gmail.com>
2020-08-23 16:31:39 +0200

    adding single product detail

--- util/database.js
+++ util/database.js
@@ -1,17 +1,29 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = callback => {
   MongoClient.connect(
     'mongodb+srv://tansen:AV5HKRcDzOuzrjRf@poko.k0ese.mongodb.net/poko?retryWrites=true&w=majority'
   )
     .then(client => {
       console.log('Connected!');
-      callback(client);
+      _db = client.db();
+      callback();
     })
     .catch(err => {
       console.log(err);
+      throw err;
     });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
