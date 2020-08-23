5c477946c5f4c5afba0f0bb4e485b53c56aa7f0b
tansen <tansenkhan1990@gmail.com>
2020-08-23 14:03:16 +0200

    connection with mongodb with atlas

--- util/database.js
+++ util/database.js
@@ -1,8 +1,14 @@
-const Sequelize = require('sequelize');
+const mongodb=require('mongodb');
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const MongoClient=mongodb.MongoClient;
 
-module.exports = sequelize;
+mongoConnect=callback=>{
+  MongoClient.connect('mongodb+srv://tansen:AV5HKRcDzOuzrjRf@poko.k0ese.mongodb.net/poko?retryWrites=true&w=majority').
+  then(client=>{
+    console.log('connected');
+    callback(client);
+  }).catch(err=>{
+    console.log('error');
+  })
+}
+module.exports=mongoConnect;
