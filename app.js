ade7f7c607969b76ffa2006c5d579bace8e1b998
tansen <tansenkhan1990@gmail.com>
2020-08-24 16:48:36 +0200

    adding product with userId reference

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('5baa2528563f16379fc8a610')
+  User.findById('5f43d256737382ff10b14d18')
     .then(user => {
       req.user = user;
       next();
