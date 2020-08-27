d16ce713811568167e0e1abfd6666f94bf49cc75
tansen <tansenkhan1990@gmail.com>
2020-08-27 18:26:11 +0200

    adding login functionality and all are working without error

--- poko.js
+++ poko.js
@@ -1,2 +1,2 @@
-mongodb+srv://tansen:pingpong@poko.k0ese.mongodb.net/poko?retryWrites=true&w=majority
+'mongodb+srv://tansen:pingpong@poko.k0ese.mongodb.net/poko?retryWrites=true&w=majority';
 5f4535616de7ee63059dfcc5
